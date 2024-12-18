815306ba35a4d2c10368eb177d9cd1bdff414f1e
Baharian Diko <bahariandikoo@gmail.com>
2024-12-18 14:41:09 +0700

    fix error in dto

--- db/db.js
+++ db/db.js
@@ -1,7 +1,5 @@
-const { Pool } = require("pg");
-
 const Pool = require("pg").Pool;
-const Pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 // const pool = new Pool({
