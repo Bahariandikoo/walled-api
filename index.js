15de132765acec973b2b0ce246ca3024bc9563e5
Baharian Diko <bahariandikoo@gmail.com>
2024-12-18 15:04:46 +0700

    fix index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
-var cors = require("cors");
 
 const userRouter = require("./routers/users.router");
+const transactionRouter = require("./routers/transactions.router");
+const globalErrorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 const port = process.env.APP_PORT;
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -15,8 +17,10 @@ app.use(
   })
 );
 
-app.use(cors());
 app.use(userRouter);
+app.use(transactionRouter);
+
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
